2be36373c86bbb479b90a72e47e48c3ec6e1a61e
thematrixa <ivanov960806@gmail.com>
2017-10-15 16:12:25 +0300

    f
    f

--- src/main/java/com/example/GorchyGames/DemoApplication.java
+++ src/main/java/com/example/GorchyGames/DemoApplication.java
@@ -2,8 +2,11 @@ package com.example.GorchyGames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
